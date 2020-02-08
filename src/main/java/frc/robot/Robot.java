39da01c4f2c279277ba43850b62eb63ec75fcb3f
Alannah Napast <alannah.napast@gmail.com>
2020-02-08 12:26:39 -0500

    color sensor stuff

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,8 +7,14 @@
 
 package frc.robot;
 
+import com.revrobotics.ColorMatch;
+import com.revrobotics.ColorMatchResult;
+import com.revrobotics.ColorSensorV3;
+
+import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
@@ -24,6 +30,15 @@ public class Robot extends TimedRobot {
 
   private RobotContainer robotContainer;
 
+
+  //color sensor stuff (im sorry its in robot please move it)
+  private final I2C.Port i2cPort = I2C.Port.kOnboard;
+ private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
+ private final ColorMatch m_colorMatcher = new ColorMatch();
+ private final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
+ private final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
+ private final Color kRedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
+ private final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
@@ -33,6 +48,12 @@ public class Robot extends TimedRobot {
   
     robotContainer = new RobotContainer();
     robotContainer.drive.zeroNavx();
+
+    //more color stuff, please move
+    m_colorMatcher.addColorMatch(kBlueTarget);
+    m_colorMatcher.addColorMatch(kGreenTarget);
+    m_colorMatcher.addColorMatch(kRedTarget);
+    m_colorMatcher.addColorMatch(kYellowTarget);    
   }
 
   /**
@@ -46,6 +67,32 @@ public class Robot extends TimedRobot {
   public void robotPeriodic() {
     
     CommandScheduler.getInstance().run();
+
+    Color detectedColor = m_colorSensor.getColor();
+
+    String colorString;
+    ColorMatchResult match = m_colorMatcher.matchClosestColor(detectedColor);
+
+    if (match.color == kBlueTarget) {
+      colorString = "Blue";
+    } else if (match.color == kRedTarget) {
+      colorString = "Red";
+    } else if (match.color == kGreenTarget) {
+      colorString = "Green";
+    } else if (match.color == kYellowTarget) {
+      colorString = "Yellow";
+    } else {
+      colorString = "Unknown";
+    }
+
+    SmartDashboard.putNumber("Red", detectedColor.red);
+    SmartDashboard.putNumber("Green", detectedColor.green);
+    SmartDashboard.putNumber("Blue", detectedColor.blue);
+    SmartDashboard.putNumber("Confidence", match.confidence);
+    SmartDashboard.putString("Detected Color", colorString);
+  
+
+
 
     /**
      * place any SmartDashboard methods that should be running even when the robot is disabled here
