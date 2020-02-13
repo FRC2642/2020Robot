dc35cf1d6cc903add452592fccfbf3df5a4b3609
Joseph Sowers <43393223+Mahjongasaurus@users.noreply.github.com>
2020-02-13 18:54:00 -0500

    dont worry about this

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -7,7 +7,6 @@
 
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.revrobotics.CANEncoder;
 import com.revrobotics.CANPIDController;
 import com.revrobotics.CANSparkMax;
